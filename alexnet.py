4353b1c3d2d3768d55a2b2b09b65e1353deb93dc
Paolo Gastaldi <61888083+paologastaldi-polito@users.noreply.github.com>
2020-12-12 15:57:21 +0100

    fix: small issues

--- alexnet.py
+++ alexnet.py
@@ -1,8 +1,12 @@
 import torch
 import torch.nn as nn
-from .utils import load_state_dict_from_url
+# from .utils import load_state_dict_from_url
 from typing import Any
 
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 __all__ = ['AlexNet', 'alexnet']
 
